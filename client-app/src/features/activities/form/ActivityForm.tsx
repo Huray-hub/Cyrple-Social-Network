eb8f04b1dbaebd4b1485fd09c2f52f24d600e458
Pskiad17 <panos.skiadas12@gmail.com>
2019-09-20 03:15:19 +0300

    persisted data back to the API
    Note: I 've added some delay between responses, in order to create some loading indicators to buttons and page itself.

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,13 +8,15 @@ interface IProps {
   activity: IActivity;
   createActivity: (activity: IActivity) => void;
   editActivity: (activity: IActivity) => void;
+  submitting: boolean;
 }
 
 const ActivityForm: React.FC<IProps> = ({
   setEditMode,
   activity: initialFormState,
   createActivity,
-  editActivity
+  editActivity,
+  submitting
 }) => {
   const InitializeForm = () => {
     if (initialFormState) {
@@ -40,7 +42,7 @@ const ActivityForm: React.FC<IProps> = ({
         ...activity,
         id: uuid()
       };
-      createActivity(activity);
+      createActivity(newActivity);
     } else {
       editActivity(activity);
     }
@@ -94,7 +96,13 @@ const ActivityForm: React.FC<IProps> = ({
           placeholder="Venue"
           value={activity.venue}
         />
-        <Button floated="right" positive type="submit" content="Submit" />
+        <Button
+          loading={submitting}
+          floated="right"
+          positive
+          type="submit"
+          content="Submit"
+        />
         <Button
           onClick={() => setEditMode(false)}
           floated="right"
