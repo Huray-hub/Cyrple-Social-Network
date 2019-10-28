cde38e22ca93e53c3a25cea7396168043523cce4
Pskiad17 <panos.skiadas12@gmail.com>
2019-10-28 05:09:52 +0200

    Photo Upload - using Cloudinary as Cloud Service (server-side)

--- Cyrple/Application/Activities/MappingProfile.cs
+++ Cyrple/Application/Activities/MappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Dtos;
 using AutoMapper;
 using Domain;
+using System.Linq;
 
 namespace Application.Activities
 {
@@ -11,7 +12,9 @@ namespace Application.Activities
             CreateMap<Activity, ActivityDto>();
             CreateMap<UserActivity, AttendeeDto>()
                 .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
-                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName));
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x 
+                    => x.IsMain).Url));
         }
     }
 }
--- Cyrple/Domain/AppUser.cs
+++ Cyrple/Domain/AppUser.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Domain
 {
     public class AppUser : IdentityUser
     {
         public string DisplayName { get; set; }
-
+        public string Bio { get; set; }
         public virtual ICollection<UserActivity> UserActivities { get; set; }
+        public virtual ICollection<Photo> Photos { get; set; } 
     }
 }
