10cf7c915a347a86f3081b00ef2549c595545963
Pskiad17 <panos.skiadas12@gmail.com>
2019-11-14 02:10:17 +0200

    -

--- Cyrple/Application/Activities/Unattend.cs
+++ Cyrple/Application/Activities/Unattend.cs
@@ -4,9 +4,7 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 using System;
-using System.Collections.Generic;
 using System.Net;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
--- Cyrple/Application/User/Register.cs
+++ Cyrple/Application/User/Register.cs
@@ -8,10 +8,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
